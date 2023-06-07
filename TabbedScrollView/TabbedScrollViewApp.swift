bd8bba0fd718aa7af9279e04ac2ace4e4bcc16fb
Vina Melody <vina_melody@yahoo.com>
2023-06-07 18:19:52 +0800

    Initial Commit

---
+++ TabbedScrollView/TabbedScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabbedScrollViewApp.swift
+//  TabbedScrollView
+//
+//  Created by Vina Rianti on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct TabbedScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
